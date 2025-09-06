c42fb251f722d0f83bb914077ad4fee48b312f59
palakptl04 <palakprashantpatel2021@gmail.com>
2025-09-06 17:19:32 +0530

    Time: 35 ms (76.13%), Space: 53.5 MB (76.47%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string> > ret;
-        if(s.empty()) return ret;
-        
+        vector<vector<string>> result;
         vector<string> path;
-        dfs(0, s, path, ret);
-        
-        return ret;
+        backtrack(s, 0, path, result);
+        return result;
     }
-    
-    void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret) {
-        if(index == s.size()) {
-            ret.push_back(path);
+
+private:
+    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result) {
+        // If we've reached the end of the string, add the current partition to the result list
+        if (start == s.length()) {
+            result.push_back(path);
             return;
         }
-        for(int i = index; i < s.size(); ++i) {
-            if(isPalindrome(s, index, i)) {
-                path.push_back(s.substr(index, i - index + 1));
-                dfs(i+1, s, path, ret);
+        // Explore all possible partitions
+        for (int end = start + 1; end <= s.length(); ++end) {
+            // If the current substring is a palindrome, add it to the current path
+            if (isPalindrome(s, start, end - 1)) {
+                path.push_back(s.substr(start, end - start));
+                // Recur to find other partitions
+                backtrack(s, end, path, result);
+                // Backtrack to explore other partitions
                 path.pop_back();
             }
         }
     }
-    
-    bool isPalindrome(const string& s, int start, int end) {
-        while(start <= end) {
-            if(s[start++] != s[end--])
+
+    bool isPalindrome(const string& s, int left, int right) {
+        // Check if the substring s[left:right+1] is a palindrome
+        while (left < right) {
+            if (s[left++] != s[right--]) {
                 return false;
+            }
         }
         return true;
     }
