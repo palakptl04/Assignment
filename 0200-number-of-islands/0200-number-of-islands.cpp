2024b54e6f51f975c5fa51fd824f933192e5e937
palakptl04 <palakprashantpatel2021@gmail.com>
2025-09-18 21:09:27 +0530

    Time: 25 ms (72.49%), Space: 22.6 MB (38.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
-        
-        int numIslands = 0;
         int m = grid.size();
+        if (m == 0) return 0;
         int n = grid[0].size();
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        queue<pair<int, int>> q;
-        
+
+        int islands = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    numIslands++;
-                    q.push({i, j});
-                    
-                    while (!q.empty()) {
-                        auto [x, y] = q.front();
-                        q.pop();
-                        
-                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') {
-                            continue;
-                        }
-                        
-                        grid[x][y] = '0'; // mark as visited
-                        
-                        for (auto& dir : directions) {
-                            int nx = x + dir.first;
-                            int ny = y + dir.second;
-                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
-                                q.push({nx, ny});
-                            }
-                        }
-                    }
+                    islands++;
+                    sinkIsland(grid, i, j, m, n);
                 }
             }
         }
-        
-        return numIslands;
+        return islands;
+    }
+
+private:
+    void sinkIsland(vector<vector<char>>& grid, int i, int j, int m, int n) {
+        stack<pair<int,int>> st;
+        st.push({i,j});
+        grid[i][j] = '0';
+
+        while (!st.empty()) {
+            auto [x,y] = st.top(); st.pop();
+
+            if (x > 0 && grid[x-1][y] == '1') {
+                grid[x-1][y] = '0';
+                st.push({x-1,y});
+            }
+            if (x < m-1 && grid[x+1][y] == '1') {
+                grid[x+1][y] = '0';
+                st.push({x+1,y});
+            }
+            if (y > 0 && grid[x][y-1] == '1') {
+                grid[x][y-1] = '0';
+                st.push({x,y-1});
+            }
+            if (y < n-1 && grid[x][y+1] == '1') {
+                grid[x][y+1] = '0';
+                st.push({x,y+1});
+            }
+        }
     }
 };
