72bceb68f8f3de88ee186ba987298531b08634c7
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-01 21:50:36 +0530

    Time: 31 ms (77.66%), Space: 21.7 MB (85.2%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        prefix_sum = 0
-        prefix_sum_count = {0: 1}  # Initialize with prefix sum 0 and count 1
-        
-        for num in nums:
-            prefix_sum += num  # Update the running prefix sum
-            if (prefix_sum - k) in prefix_sum_count:
-                count += prefix_sum_count[prefix_sum - k]  # Increment count if (prefix_sum - k) is found
-            if prefix_sum in prefix_sum_count:
-                prefix_sum_count[prefix_sum] += 1  # Update the frequency of the current prefix sum
-            else:
-                prefix_sum_count[prefix_sum] = 1  # Initialize the frequency if the prefix sum is seen for the first time
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        sub_num = {0:1}
+        total = count = 0
+
+        for n in nums:
+            total += n
+            
+            if total - k in sub_num:
+                count += sub_num[total-k]
+            
+            sub_num[total] = 1 + sub_num.get(total, 0)
         
         return count
