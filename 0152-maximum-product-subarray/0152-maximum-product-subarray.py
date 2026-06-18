7f467c0758371958dcdb9fd4496123fae77ac6df
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-18 17:27:30 +0530

    Time: 0 ms (100%), Space: 19.9 MB (44.62%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,21 +1,14 @@
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
         maxi = float('-inf')
-        prod = 1
-
-        # Forward pass
-        for num in nums:
-            prod *= num
-            maxi = max(prod, maxi)
-            if prod == 0:
-                prod = 1
-
-        prod = 1
-        # Backward pass
-        for num in reversed(nums):
-            prod *= num
-            maxi = max(prod, maxi)
-            if prod == 0:
-                prod = 1
-
+        prefix = 0
+        suffix = 0
+        
+        for i in range(len(nums)):
+            # If previous product was 0, reset to 1, otherwise multiply by current number
+            prefix = (prefix or 1) * nums[i]
+            suffix = (suffix or 1) * nums[~i]  # ~i counts backwards from the end
+            
+            maxi = max(maxi, prefix, suffix)
+            
         return maxi
