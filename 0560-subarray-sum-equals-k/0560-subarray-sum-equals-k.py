56ebca317691b486c64cf93bcccfb8e99355f32c
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-18 13:37:44 +0530

    Time: 30 ms (80.25%), Space: 21.7 MB (85.31%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -6,13 +6,16 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        sums = 0
-        d = dict()
-        d[0] = 1
+        prefix_sum = 0
+        prefix_sum_count = {0: 1}  # Initialize with prefix sum 0 and count 1
         
-        for i in range(len(nums)):
-            sums += nums[i]
-            count += d.get(sums-k,0)
-            d[sums] = d.get(sums,0) + 1
+        for num in nums:
+            prefix_sum += num  # Update the running prefix sum
+            if (prefix_sum - k) in prefix_sum_count:
+                count += prefix_sum_count[prefix_sum - k]  # Increment count if (prefix_sum - k) is found
+            if prefix_sum in prefix_sum_count:
+                prefix_sum_count[prefix_sum] += 1  # Update the frequency of the current prefix sum
+            else:
+                prefix_sum_count[prefix_sum] = 1  # Initialize the frequency if the prefix sum is seen for the first time
         
-        return(count)
+        return count
