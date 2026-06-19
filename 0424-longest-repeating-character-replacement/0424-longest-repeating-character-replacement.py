a5606d26b225830471eea3caec6a1a026300d062
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-19 18:06:25 +0530

    Time: 83 ms (58.98%), Space: 19.7 MB (50.4%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,24 @@
-from collections import defaultdict
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freqs = defaultdict(int)
-        res = 0
-        i = 0
+        left = 0
+        max_freq = 0
+        frequency = {}
+        longest_str_len = 0
 
-        for j in range(len(s)):
-            freqs[s[j]] += 1
-            maxFreq = max(freqs.values())
-            curLen = j - i + 1
-            if curLen - maxFreq > k:
-                freqs[s[i]] -= 1
-                i += 1
-            res = max(res, j - i + 1)
-        
-        return res
+        for right in range(len(s)):
+            # Expand the window
+            frequency[s[right]] = frequency.get(s[right], 0) + 1
+            max_freq = max(max_freq, frequency[s[right]])
+
+            # Shrink the window if the condition is violated
+            # (cells count - max frequency of any character seen > k)
+            while (right - left + 1) - max_freq > k:
+                frequency[s[left]] -= 1
+                if frequency[s[left]] == 0:
+                    del frequency[s[left]]
+                left += 1
+
+            # Update the result if the current window is larger
+            longest_str_len = max(longest_str_len, right - left + 1)
+
+        return longest_str_len
