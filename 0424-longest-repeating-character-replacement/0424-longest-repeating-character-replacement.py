ec1e613437a533d5959ae6734e2c9604f2daff08
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-19 18:07:40 +0530

    Time: 77 ms (80.1%), Space: 19.6 MB (50.4%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,24 +1,12 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        left = 0
-        max_freq = 0
-        frequency = {}
-        longest_str_len = 0
-
-        for right in range(len(s)):
-            # Expand the window
-            frequency[s[right]] = frequency.get(s[right], 0) + 1
-            max_freq = max(max_freq, frequency[s[right]])
-
-            # Shrink the window if the condition is violated
-            # (cells count - max frequency of any character seen > k)
-            while (right - left + 1) - max_freq > k:
-                frequency[s[left]] -= 1
-                if frequency[s[left]] == 0:
-                    del frequency[s[left]]
-                left += 1
-
-            # Update the result if the current window is larger
-            longest_str_len = max(longest_str_len, right - left + 1)
-
-        return longest_str_len
+class Solution: 
+    def characterReplacement(self, s, k):
+        maxf = res = 0
+        count = collections.Counter()
+        for i in range(len(s)):
+            count[s[i]] += 1
+            maxf = max(maxf, count[s[i]])
+            if res - maxf < k:
+                res += 1
+            else:
+                count[s[i - res]] -= 1
+        return res
