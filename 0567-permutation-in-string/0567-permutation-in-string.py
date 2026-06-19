3c42a1f4fadc5ee6905cf28fbb5a111280abbeea
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-19 19:34:35 +0530

    Time: 31 ms (38.52%), Space: 19.5 MB (18.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+	    cntr, w, match = Counter(s1), len(s1), 0     
 
-        s1_count = {}
-        s2_count = {}
+	    for i in range(len(s2)):
+		    if s2[i] in cntr:
+			    if not cntr[s2[i]]: match -= 1
+			    cntr[s2[i]] -= 1
+			    if not cntr[s2[i]]: match += 1
 
-        for i in range(len(s1)):
-            s1_count[s1[i]] = 1 + s1_count.get(s1[i], 0)
-            s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
+		    if i >= w and s2[i-w] in cntr:
+			    if not cntr[s2[i-w]]: match -= 1
+			    cntr[s2[i-w]] += 1
+			    if not cntr[s2[i-w]]: match += 1
 
-        if s1_count == s2_count:
-            return True
+		    if match == len(cntr):
+			    return True
 
-        left = 0
-        for right in range(len(s1), len(s2)):
-            s2_count[s2[right]] = 1 + s2_count.get(s2[right], 0)
-            s2_count[s2[left]] -= 1
-
-            if s2_count[s2[left]] == 0:
-                del s2_count[s2[left]]
-
-            left += 1
-
-            if s1_count == s2_count:
-                return True
-
-        return False
+	    return False
