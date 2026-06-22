b6793ec07ebbab05da7cac4b5410a2d21fea8e51
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-22 22:31:10 +0530

    Time: 14 ms (22.23%), Space: 30.4 MB (78.22%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -4,7 +4,10 @@ class Solution:
         for r in range(len(nums)):
             s += nums[r]
             while s >= target:
-                m = min(m, r - l + 1)
+                current_len = r - l + 1
+                if current_len < m:
+                    m = current_len
+               
                 s -= nums[l]
                 l += 1
         return 0 if m == float('inf') else m
