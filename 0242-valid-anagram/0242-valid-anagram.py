378374fa43475ee14695d3138f35c10b93a43b2e
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-24 10:15:17 +0530

    Time: 15 ms (43.85%), Space: 19.5 MB (44.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,6 +7,5 @@ class Solution:
         s2={}
         for i in range(len(s)):
             s1[s[i]]=s1.get(s[i],0)+1
-        for i in range(len(t)):
             s2[t[i]]=s2.get(t[i],0)+1
         return s1==s2
