23418231b819cebb61b0701ce0318785679ee70d
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-24 22:02:51 +0530

    Time: 18 ms (23.14%), Space: 23.8 MB (31.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,7 +3,10 @@ class Solution:
         ans = defaultdict(list)
 
         for s in strs:
-            key = "".join(sorted(s))
-            ans[key].append(s)
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
         
         return list(ans.values())
