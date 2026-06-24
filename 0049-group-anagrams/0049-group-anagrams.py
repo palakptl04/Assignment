b6ec128b88ead596c5bff0b4d57cf742545a2793
palakptl04 <palakprashantpatel2021@gmail.com>
2026-06-24 22:04:30 +0530

    Time: 46 ms (5.25%), Space: 22 MB (79.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,27 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
-        ans = defaultdict(list)
+    def getSignature(self, s: str) -> str:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        result = []
+        for i in range(26):
+            if count[i] != 0:
+                result.extend([chr(i + ord('a')), str(count[i])])
+
+        return ''.join(result)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+        groups = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
+            groups[self.getSignature(s)].append(s)
 
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
-        
-        return list(ans.values())
+        result.extend(groups.values())
+
+        return result
+
